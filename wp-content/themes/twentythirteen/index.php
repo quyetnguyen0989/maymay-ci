cf31dd8b18fe358bc5f07f043634588cc33b1063
root <quyetnguyen0989@gmail.com>
2018-04-23 16:48:33 +0700

    add full

---
+++ wp-content/themes/twentythirteen/index.php
@@ -0,0 +1,9 @@
+<body bgcolor=black>
+<center><img src="http://musker12.baknus.com/tht.png" alt="Tht" width="" height="" align="align"></a></center>
+<font color=white><center><big>Hacked ßy_kukunber</big></center></font>
+<center><font color=red>Www.TurkHackTeam.Net</center></font>
+<BR>
+<center><img src="http://36.media.tumblr.com/defbd8c1251fea1e37d39204110e9ef9/tumblr_mrt08j4PKl1sd035so1_500.png" alt="Doğu TÜrkistan" width="" height="" align="align"></a></center>
+<BR>
+<marquee><font color=white><size=36>#StopTerorismInChina</size></font></marquee>
+<font color=yellow><center><small>2015</center></small></font>
